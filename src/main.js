944310b12fa58fa16579c4c63e3cc36e461d4f4b
Nathaniel <nathanielmujesia@gmail.com>
2026-04-15 20:41:08 +0300

    feat: add view patient action modal markup

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@
 // DOM ELEMENTS
 // ======================
 const addPatientForm = document.querySelector("#addPatientForm");
-const patientTableSearchInput = document.querySelector('#patientTableSearchInput');
-const statusFilter = document.querySelector('#statusFilter');
-const genderFilter = document.querySelector('#genderFilter');
+const patientTableSearchInput = document.querySelector(
+    "#patientTableSearchInput",
+);
+const statusFilter = document.querySelector("#statusFilter");
+const genderFilter = document.querySelector("#genderFilter");
 const tableBody = document.querySelector("#patientsTableBody");
 
 // ======================
 // APPLICATION STATE
 // ======================
-const allPatientsList  = [];
+let allPatientsList = [];
 
 // ======================
 // APPLICATION STATE
@@ -20,8 +22,6 @@ const BASE_URL = "http://localhost:3000/patients";
 // ======================
 // UTILITIES
 // ======================
-
-
 // Calculate patient age from date of birth
 const calculateAge = (dateOfBirth) => {
     const today = new Date();
@@ -29,90 +29,170 @@ const calculateAge = (dateOfBirth) => {
     let age = today.getFullYear() - birthDate.getFullYear();
     const monthDifference = today.getMonth() - birthDate.getMonth();
 
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+    if (
+        monthDifference < 0 ||
+        (monthDifference === 0 && today.getDate() < birthDate.getDate())
+    ) {
         age--;
     }
     return age;
-}
+};
 
 // ======================
 // API CALLS
 // ======================
 // Fetch all patients
 const fetchPatients = async () => {
-    try{
+    try {
         const response = await fetch(BASE_URL);
-        if(!response.ok){
-            throw new Error("Failed to fetch patients, status:" + response.status);
+        if (!response.ok) {
+            throw new Error(
+                "Failed to fetch patients, status:" + response.status,
+            );
         }
-        const patients = await response.json();
-        allPatientsList.push(...patients);
-        updateUI();                
 
-    }catch(error){
+        const patients = await response.json();
+        const orderOfPatients = await patients.reverse();
+        allPatientsList.push(...orderOfPatients);
+        updateUI();
+    } catch (error) {
         console.error("Error fetching patients:", error.message);
     }
-}
+};
 
 // Create new patient
 const postPatient = async (newPatientData) => {
-    try{
-        const response = await fetch(BASE_URL,{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
+    try {
+        const response = await fetch(BASE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify(newPatientData)
+            body: JSON.stringify(newPatientData),
         });
-        if(!response.ok) throw new Error("Failed to create patient, status:" + response.status);
-    const newPatient = await response.json()
-    allPatientsList.unshift(newPatient)
-    updateUI();
-
-    }catch(error){
+        if (!response.ok)
+            throw new Error(
+                "Failed to create patient, status: " + response.status,
+            );
+        const newPatient = await response.json();
+        allPatientsList.unshift(newPatient);
+        updateUI();
+    } catch (error) {
         console.error("Error:", error.message);
     }
-}
+};
+
+/* 
+User clicks delete
+   ↓
+Call API (DELETE)
+   ↓
+Update local state (patients)
+   ↓
+Re-run filters/search
+   ↓
+Re-render UI
+*/
+
+// Delete Patient
+const deletePatient = async (id) => {
+    try {
+        const response = await fetch(`${BASE_URL}/${id}`, {
+            method: "DELETE",
+        });
+        if (!response.ok)
+            throw new Error(
+                "Failed to delete patient, status: " + response.status,
+            );
+
+        return true;
+    } catch (error) {
+        console.error("Error deleting patient:" + error.message);
+    }
+};
+
+/*
+User edits form
+   ↓
+Call API (PUT/PATCH)
+   ↓
+Update local state (replace item)
+   ↓
+Re-run filters/search
+   ↓
+Re-render UI
+
+*/
+
+// ======================
+// CONTROLLERS
+// ======================
+const handleView = (id) => {
+    console.log("chillllll!!!!");
+};
+const handleEdit = (id) => {
+    console.log("chillllll-edit-ax!!!!");
+};
+const handleDelete = async (id) => {
+    const patient = allPatientsList.find((p) => p.id === id);
+
+    if (!patient) return;
+
+    const confirmed = confirm(
+        `Are you sure you want to delete ${patient.name}? This action cannot be undone.`,
+    );
+
+    if (!confirmed) return;
+
+    try {
+         await deletePatient(id);
+        
+        allPatientsList = allPatientsList.filter((p) => p.id !== id);
+
+        updateUI();
+    } catch (error) {
+        console.error(`Error deleting patient, Error: ${error.message}`);
+    }
+};
 
 // ======================
 // DATA PROCESSING (SEARCH / FILTER / SORT)
 // ======================
 const applyFilterAndRender = () => {
-    let result = [...allPatientsList];    
+    let result = [...allPatientsList];
     const searchValue = patientTableSearchInput.value.toLowerCase();
     const status = statusFilter.value.toLowerCase();
     const gender = genderFilter.value.toLowerCase();
-    console.log(genderFilter.value)
-    // Search 
-    if(searchValue){
-        result = result.filter( p => p.name.toLowerCase().includes(searchValue))
+
+    // Search
+    if (searchValue) {
+        result = result.filter((p) =>
+            p.name.toLowerCase().includes(searchValue),
+        );
     }
-    // filter by status 
-    if(status !== 'all'){
-        result = result.filter(p => p.status.toLowerCase().includes(status))   
-        
+    // filter by status
+    if (status !== "all") {
+        result = result.filter((p) => p.status.toLowerCase().includes(status));
     }
     // filter by gender
-    if(gender !== 'all'){
-        result = result.filter(p => p.gender.toLowerCase().includes(gender))   
-        
+    if (gender !== "all") {
+        result = result.filter((p) => p.gender.toLowerCase().includes(gender));
     }
     displayPatients(result);
-}
+};
 
 // ======================
 // UI RENDERING
 // ======================
 const displayPatients = (patients) => {
-    tableBody.innerHTML = ""; 
+    tableBody.innerHTML = "";
 
-    patients.forEach( patient => {
-    const tableRow = document.createElement("tr");
-    tableRow.classList.add("patient-record");
-    tableRow.setAttribute("role", "row");    
-    tableRow.innerHTML = `
-    <tr>
-        <td class="visually-hidden hidden" role="cell">${patient.id}</td>
+    patients.reverse().forEach((patient) => {
+        const tableRow = document.createElement("tr");
+        tableRow.classList.add("patient-record");
+        tableRow.setAttribute("role", "row");
+        tableRow.innerHTML = `
+    <tr>        
         <td role="cell">${patient.name}</td>
         <td class="numeric" role="cell">${calculateAge(patient.dateOfBirth)}</td>
         <td role="cell">${patient.condition}</td>
@@ -120,19 +200,19 @@ const displayPatients = (patients) => {
         <td role="cell">
             <div class="patients-table-action-buttons">
                 <!-- view -->
-                <div class="actions-icon-wraper">
-                    <svg class=" patients-table-actions-view-icon | patients-table-actions-icon">
+                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="view">
+                    <svg class=" patients-table-actions-view-icon | patients-table-actions-icon" >
                         <use xlink:href="../src/assets/images/misc/misc.svg#view"></use>
                     </svg>
                 </div>
                 <!-- edit -->
-                <div class="actions-icon-wraper">
+                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="edit">
                     <svg class="patients-table-actions-edit-icon | patients-table-actions-icon">
                         <use xlink:href="../src/assets/images/misc/misc.svg#edit"></use>
                     </svg>
                 </div>
                 <!-- delete -->
-                <div class="actions-icon-wraper">
+                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="delete">
                     <svg class="patients-table-actions-delete-icon | patients-table-actions-icon">
                         <use xlink:href="../src/assets/images/misc/misc.svg#delete"></use>
                     </svg>
@@ -141,17 +221,17 @@ const displayPatients = (patients) => {
         </td>
     </tr>    
     `;
-    tableBody.prepend(tableRow);
-    })    
-}
+        tableBody.prepend(tableRow);
+    });
+};
 
 // ======================
 // CONTROLLER
 // ======================
 // Central UI updater (single pipeline)
-const updateUI = () =>{
+const updateUI = () => {
     applyFilterAndRender();
-}
+};
 
 // ======================
 // EVENT LISTENERS
@@ -160,27 +240,40 @@ const updateUI = () =>{
 patientTableSearchInput.addEventListener("input", updateUI);
 
 // Filters
-statusFilter.addEventListener('change', updateUI);
-genderFilter.addEventListener('change', updateUI)
-
+statusFilter.addEventListener("change", updateUI);
+genderFilter.addEventListener("change", updateUI);
 
 //  Form Submit (patient)
-addPatientForm.addEventListener("submit",  (e) => {
+addPatientForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const newPatientData = {
-            name: e.target.name.value,
-            gender: e.target.gender.value,
-            dateOfBirth: e.target.dateOfBirth.value,
-            email: e.target.email.value,
-            phone: e.target.phone.value,
-            condition: e.target.condition.value,
-            status: e.target.status.value,
-            lastVisit: e.target.lastVisit.value,
-            notes: e.target.notes.value  
-    }
-    postPatient(newPatientData)
-    })      
+        name: e.target.name.value,
+        gender: e.target.gender.value,
+        dateOfBirth: e.target.dateOfBirth.value,
+        email: e.target.email.value,
+        phone: e.target.phone.value,
+        condition: e.target.condition.value,
+        status: e.target.status.value,
+        lastVisit: e.target.lastVisit.value,
+        notes: e.target.notes.value,
+    };
+    postPatient(newPatientData);
+});
+
+tableBody.addEventListener("click", (e) => {
+    const wrapper = e.target.closest(".actions-icon-wrapper");
+    
+
+    if (!wrapper) return;
+
+    const id = wrapper.dataset.id;
+    const action = wrapper.dataset.action;
+
+    if (action === "view") handleView(id);
+    if (action === "edit") handleEdit(id);
+    if (action === "delete") handleDelete(id);
+});
 
 // ======================
 // INIT
@@ -191,4 +284,3 @@ const init = () => {
 };
 
 init();
-
