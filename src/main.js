6adac814c9b0e6552e8793c8c4f49b85bebfc14f
Nathaniel <nathanielmujesia@gmail.com>
2026-04-17 12:09:19 +0300

    feat: add view modal functionality

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ const tableBody = document.querySelector("#patientsTableBody");
 let allPatientsList = [];
 
 // ======================
-// APPLICATION STATE
+// 
 // ======================
 const BASE_URL = "http://localhost:3000/patients";
 
@@ -37,6 +37,14 @@ const calculateAge = (dateOfBirth) => {
     }
     return age;
 };
+
+// Format date (year month day: Feb 10, 2025 )
+  const formatDate = (date) =>
+    new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
 
 // ======================
 // API CALLS
@@ -82,17 +90,7 @@ const postPatient = async (newPatientData) => {
     }
 };
 
-/* 
-User clicks delete
-   ↓
-Call API (DELETE)
-   ↓
-Update local state (patients)
-   ↓
-Re-run filters/search
-   ↓
-Re-render UI
-*/
+
 
 // Delete Patient
 const deletePatient = async (id) => {
@@ -128,8 +126,14 @@ Re-render UI
 // CONTROLLERS
 // ======================
 const handleView = (id) => {
-    console.log("chillllll!!!!");
+    const patient = allPatientsList.find(p => p.id == id);
+
+    if(patient){
+        openViewModal(patient)
+        
+    }
 };
+
 const handleEdit = (id) => {
     console.log("chillllll-edit-ax!!!!");
 };
@@ -191,8 +195,7 @@ const displayPatients = (patients) => {
         const tableRow = document.createElement("tr");
         tableRow.classList.add("patient-record");
         tableRow.setAttribute("role", "row");
-        tableRow.innerHTML = `
-    <tr>        
+        tableRow.innerHTML = `           
         <td role="cell">${patient.name}</td>
         <td class="numeric" role="cell">${calculateAge(patient.dateOfBirth)}</td>
         <td role="cell">${patient.condition}</td>
@@ -200,30 +203,116 @@ const displayPatients = (patients) => {
         <td role="cell">
             <div class="patients-table-action-buttons">
                 <!-- view -->
-                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="view">
+                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="view" role="button" arial-label="view patient details">
                     <svg class=" patients-table-actions-view-icon | patients-table-actions-icon" >
                         <use xlink:href="../src/assets/images/misc/misc.svg#view"></use>
                     </svg>
                 </div>
                 <!-- edit -->
-                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="edit">
+                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="edit" role="button" arial-label="edit patient details">
                     <svg class="patients-table-actions-edit-icon | patients-table-actions-icon">
                         <use xlink:href="../src/assets/images/misc/misc.svg#edit"></use>
                     </svg>
                 </div>
                 <!-- delete -->
-                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="delete">
+                <div class="actions-icon-wrapper" data-id="${patient.id}" data-action="delete" role="button" arial-label="delete patient data">
                     <svg class="patients-table-actions-delete-icon | patients-table-actions-icon">
                         <use xlink:href="../src/assets/images/misc/misc.svg#delete"></use>
                     </svg>
                 </div>
             </div>
-        </td>
-    </tr>    
+        </td>       
     `;
         tableBody.prepend(tableRow);
     });
 };
+
+const openViewModal = (patient) =>{
+    const{
+        name,
+        dateOfBirth,
+        email,
+        phone,
+        condition,
+        status,
+        lastVisit,
+        notes
+        } = patient;
+
+    let gender = patient.gender
+    if(gender ==='man') gender="male"
+
+    const age = calculateAge(dateOfBirth);
+
+    const viewDialog = document.querySelector("#viewModal");
+
+    viewDialog.innerHTML = `<article>
+    <!-- HEADER -->
+    <header class="view-modal-header">
+      <h2 class="modal-title | fw-bold" id="viewModalTitle">Patient Details</h2>
+      <button commandfor="viewModal" command="close" class="btn" aria-label="close dialog" data-type="close-modal-btn">&times;</button>
+    </header>
+    <!-- MAIN CONTENT -->
+    <div class="modal-body">
+      <!-- Identity -->
+      <section class="view-modal-patient-identity">
+        <h3 class="modal-title | fw-semi-bold">${name}</h3>
+        <p><span class="capitalize">${gender}<span/> • ${age} years</p>
+      </section>
+      <!-- Details -->
+      <div class="patient-details-wrapper">
+        <dl class="patient-details">
+          <!-- Condition , Date-of-Last-Visit  & Status Group -->
+          <div class="patient-details-group">
+                        <!-- Condition -->
+            <div class="patient-detail-wrapper">
+              <dt>Condition:</dt>
+              <dd>${condition || "N/A"}</dd>
+            </div>
+                        <!-- Date-of-Last-Visit -->
+            <div class="patient-detail-wrapper">
+      <dt>Last Visit:</dt>
+      <dd>${formatDate(lastVisit)}</dd>
+            </div>
+
+                        <!-- status -->
+            <div class="patient-detail-wrapper">
+      <dt>Status: </dt>
+      <dd class="capitalize">${status || "N/A"}</dd>
+            </div>
+          </div>
+          
+          <!-- Phone Email DOB   Group -->
+          <div class="patient-details-group">
+                      <!-- phone -->
+            <div class="patient-detail-wrapper">
+              <dt>Phone:</dt>
+              <dd>${phone || "N/A"}</dd>
+            </div>
+                      <!-- email -->
+            <div class="patient-detail-wrapper">
+              <dt>Email:</dt>
+              <dd>${email || "N/A"}</dd>
+            </div>
+            <!-- DOB -->
+            <div class="patient-detail-wrapper">
+              <dt>Date of Birth:</dt>
+              <dd>${formatDate(dateOfBirth)}</dd>
+            </div>
+          </div>
+        </dl>
+        </div>
+        <section class="patient-notes">
+  <h4>Clinical Notes</h4>
+  <p>${notes}</p>
+</section>
+      </div>    
+  </article>`    
+
+    viewDialog.showModal()
+
+
+}
 
 // ======================
 // CONTROLLER
